2d084462bc20c12738e17bc49f2d2ec4540d2a4a
tkosudi <tkosudi@gmail.com>
2022-09-29 19:05:26 -0300

    chore: add jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverage: true,
+  collectCoverage: false,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
