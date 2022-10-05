c92f5e21d9f25eddc96aa610a08e235334c5a31b
tkosudi <tkosudi@gmail.com>
2022-10-05 11:51:39 -0300

    feat: add Account Repository

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,12 +1,17 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
-  client: null as MongoClient,
+  client: null as unknown as MongoClient,
+
   async connect (uri: string): Promise<void> {
-    this.client = await MongoClient.connect(globalThis.__MONGO_URI__)
+    this.client = await MongoClient.connect(uri)
   },
 
-  async disconnect () {
+  async disconnect (): Promise<void> {
     await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
