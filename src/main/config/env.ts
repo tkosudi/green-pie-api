7855de8ccaa99678adf6d2e51b8a0ea25e2cf769
Thiago Kosudi <tkosudi@gmail.com>
2022-11-30 12:03:31 -0300

    chore: add docker-compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/green-pie-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/green-pie-api',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'tj670==5H'
 }
