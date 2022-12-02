efa3dd1ce6b3738143f1ca1d0fcd7f6c44431d4e
tkosudi <tkosudi@gmail.com>
2022-12-02 18:27:32 -0300

    refactor: add files ends with .map in to the routes.ts

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,12 +1,11 @@
 import { Express, Router } from 'express'
 import { readdirSync } from 'fs'
-import path from 'path'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  readdirSync(path.join(__dirname, '/../routes')).map(async file => {
-    if (!file.includes('.test.')) {
+  readdirSync(`${String(__dirname)}/../routes`).map(async file => {
+    if (!file.endsWith('.map') && !file.includes('.test.')) {
       (await import(`../routes/${file}`)).default(router)
     }
   })
