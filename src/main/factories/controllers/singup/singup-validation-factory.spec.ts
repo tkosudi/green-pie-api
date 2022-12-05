d7a1f98df0e70fe96609b9cb7300d646ca3f814e
tkosudi <tkosudi@gmail.com>
2022-12-05 14:48:35 -0300

    refactor: separete factories in folders

--- src/main/factories/controllers/singup/singup-validation-factory.spec.ts
+++ src/main/factories/controllers/singup/singup-validation-factory.spec.ts
@@ -1,9 +1,9 @@
-import { CompareFieldsValidation, EmailValidation, RequiredFieldValidation, ValidationComposite } from '../../../presentation/helpers/validators'
-import { EmailValidator } from '../../../presentation/protocols/email-validator'
-import { Validation } from '../../../presentation/protocols/validation'
+import { CompareFieldsValidation, EmailValidation, RequiredFieldValidation, ValidationComposite } from '../../../../presentation/helpers/validators'
+import { EmailValidator } from '../../../../presentation/protocols/email-validator'
+import { Validation } from '../../../../presentation/protocols/validation'
 import { makeSignUpValidation } from './singup-validation-factory'
 
-jest.mock('../../../presentation/helpers/validators/validation-composite.ts')
+jest.mock('../../../../presentation/helpers/validators/validation-composite.ts')
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
---
+++ src/main/factories/usecases/authentication/db-authentication-factory.ts
@@ -0,0 +1,14 @@
+import { DbAuthentication } from '../../../../data/useCases/authentication/db-authentication'
+import { JwtAdapter } from '../../../../infra/criptography/jwt-adapter/jwt-adapter'
+import { AccountMongoRepository } from '../../../../infra/db/mongodb/account/account-repository'
+import env from '../../../config/env'
+import { BcryptAdapter } from '../../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { Authentication } from '../../../../domain/useCases/authentication'
+
+export const makeDbAuthentication = (): Authentication => {
+  const salt = 12
+  const accountMongoRepository = new AccountMongoRepository()
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+  return new DbAuthentication(accountMongoRepository, bcryptAdapter, jwtAdapter, accountMongoRepository)
+}
