0436da700d41b25d84e44646494ed7acb0b6ed93
tkosudi <tkosudi@gmail.com>
2022-12-27 18:18:02 -0300

    test: ensure AddIngredientController returns a badRequest error if validation fails

--- src/presentation/controllers/login/ingredients/add-ingredient-controller.spec.ts
+++ src/presentation/controllers/login/ingredients/add-ingredient-controller.spec.ts
@@ -1,3 +1,4 @@
+import { badRequest } from './../../../helpers/http/http-helper'
 import { Validation } from './../../../protocols/validation'
 import { AddIngredientController } from './add-ingredient-controller'
 import { HttpRequest } from './add-ingredients-controller-protocols'
@@ -40,5 +41,12 @@ describe('AddIngredient Controller', () => {
     const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should return 400 if validation fails', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new Error()))
   })
 })
