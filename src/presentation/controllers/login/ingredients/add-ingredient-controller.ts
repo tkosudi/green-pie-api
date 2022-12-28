67c9fe886987d3ffd0b9307babe08859d88d54f6
tkosudi <tkosudi@gmail.com>
2022-12-28 11:15:43 -0300

    feat: ensure AddIngredientController returns 204 on success

--- src/presentation/controllers/login/ingredients/add-ingredient-controller.ts
+++ src/presentation/controllers/login/ingredients/add-ingredient-controller.ts
@@ -1,4 +1,4 @@
-import { badRequest } from './../../../helpers/http/http-helper'
+import { badRequest, noContent, serverError } from './../../../helpers/http/http-helper'
 import { Controller, HttpResponse, HttpRequest, Validation, AddIngredient } from './add-ingredients-controller-protocols'
 
 export class AddIngredientController implements Controller {
@@ -9,15 +9,19 @@ export class AddIngredientController implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const error = this.validation.validate(httpRequest.body)
-    if (error) {
-      return badRequest(error)
+    try {
+      if (error) {
+        return badRequest(error)
+      }
+      const { name, unit, amount } = httpRequest.body
+      await this.addIngredient.add({
+        name,
+        unit,
+        amount
+      })
+      return noContent()
+    } catch (error) {
+      return serverError(error)
     }
-    const { name, unit, amount } = httpRequest.body
-    await this.addIngredient.add({
-      name,
-      unit,
-      amount
-    })
-    return await new Promise(resolve => resolve(null))
   }
 }
