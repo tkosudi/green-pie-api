7c03ec8f082c7cd9c3a1573de6e5e1d59acc5c71
tkosudi <tkosudi@gmail.com>
2023-07-19 13:39:07 -0300

    test: ensure AddTransactionController return 204 on success

--- src/presentation/controllers/transactions/add-transaction-controller.spec.ts
+++ src/presentation/controllers/transactions/add-transaction-controller.spec.ts
@@ -1,6 +1,6 @@
 import { HttpRequest, Validation, AddTransaction, AddTransactionModel } from './add-transaction-controller-protocols'
 import { AddTransactionController } from './add-transaction-controller'
-import { badRequest, serverError } from '../../helpers/http/http-helper'
+import { badRequest, noContent, serverError } from '../../helpers/http/http-helper'
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
@@ -75,5 +75,11 @@ describe('Transactions Controller', () => {
     jest.spyOn(addtransactionStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('Should return 204 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(noContent())
   })
 })
