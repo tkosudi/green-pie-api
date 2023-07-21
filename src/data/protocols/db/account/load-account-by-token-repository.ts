5540482476405ef696d82b24efe49a89e11c5e3f
tkosudi <tkosudi@gmail.com>
2023-07-21 19:25:16 -0300

    feat: ensure DbLoadAccountByToken calls LoadAccountByTokenRepository with correct values

---
+++ src/data/protocols/db/account/load-account-by-token-repository.ts
@@ -0,0 +1,5 @@
+import { AccountModel } from '../../../useCases/add-account/db-add-account-protocols'
+
+export interface LoadAccountByTokenRepository {
+  loadByToken: (token: string, role?: string) => Promise<AccountModel>
+}
