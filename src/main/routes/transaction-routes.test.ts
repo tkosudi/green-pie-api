c0f904dddbe44f8d0294f9feb96e2db6a5a2b14c
tkosudi <tkosudi@gmail.com>
2023-07-22 18:02:16 -0300

    test: ensure add transaction route returns 204 if valid access token is provided

--- src/main/routes/transaction-routes.test.ts
+++ src/main/routes/transaction-routes.test.ts
@@ -1,10 +1,12 @@
-import { Collection } from 'mongodb'
+import { Collection, InsertOneResult } from 'mongodb'
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 import env from '../config/env'
+import { sign } from 'jsonwebtoken'
 
 let transactionCollection: Collection
+let accountCollection: Collection
 
 describe('Transaction Routes', () => {
   beforeAll(async () => {
@@ -18,6 +20,8 @@ describe('Transaction Routes', () => {
   beforeEach(async () => {
     transactionCollection = MongoHelper.getCollection('transactions')
     await transactionCollection.deleteMany({})
+    accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   describe('POST /transactions', () => {
@@ -32,5 +36,38 @@ describe('Transaction Routes', () => {
         })
         .expect(403)
     })
+  })
+
+  test('Should return 204 on add transaction with valid accessToken', async () => {
+    const res: InsertOneResult<Document> = await accountCollection.insertOne({
+      name: 'TK',
+      email: 'tk@gmail.com',
+      password: '123',
+      role: 'admin'
+    })
+
+    const id = res.insertedId
+    const accessToken = sign({ id }, env.jwtSecret)
+    await accountCollection.updateOne(
+      {
+        _id: id
+      },
+      {
+        $set: {
+          accessToken
+        }
+      }
+    )
+
+    await request(app)
+      .post('/api/transactions')
+      .set('x-access-token', accessToken)
+      .send({
+        type: 'credit',
+        description: 'any_description',
+        categoryId: 'any_categoryId',
+        amount: 100
+      })
+      .expect(204)
   })
 })
