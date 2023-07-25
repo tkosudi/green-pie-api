6cfe382f186d94e30c4b262a1b6a82099960da76
tkosudi <tkosudi@gmail.com>
2023-07-25 18:53:17 -0300

    chore: add env for coveralls

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,6 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/green-pie-api',
   port: process.env.PORT || 5050,
-  jwtSecret: process.env.JWT_SECRET || 'tj670==5H'
+  jwtSecret: process.env.JWT_SECRET || 'tj670==5H',
+  coverallsRepoToken: process.env.COVERALLS_REPO_TOKEN || 'PYTfVvHSxC5rEcgm7ht7BiObucFiRGUjk'
 }
